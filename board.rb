454c4487ed3d2b0257a6d9f7d2265988e261fed1
Kyle Whitaker <kylewhitaker51@gmail.com>
2017-01-09 20:15:32 -0500

    Array test working

--- board.rb
+++ board.rb
@@ -3,22 +3,22 @@ require 'minitest/autorun'
 # Board Object
 class Board
 
-  attr_accessor :cell_board, :num_rows, :num_cols
+  attr_accessor :cells, :rows, :cols
 
   def board_name
     "Conway's Game of Life"
   end
 
   def initialize(x,y)
-    @cell_board = Array.new(y){Array.new(x)}
-    @num_rows = x
-    @num_cols = y
+    @cells = Array.new(y){Array.new(x)}
+    @rows = x
+    @cols = y
   end
 
-  def start
-    @cell_board.each do |row|
-      row.each do |cell|
-        cell = true
+  def set_all_cells_alive
+    for x in 0...@rows
+      for y in 0...@cols
+        @cells[x][y] = true
       end
     end
   end
@@ -35,22 +35,23 @@ class TestBoard < MiniTest::Test
 
   def test_new_board
     board = Board.new(3,3)
-    refute_nil board.cell_board
+    refute_nil board.cells
   end
 
   def test_board_rows
     board = Board.new(5,6)
-    assert_equal 5, board.num_rows
+    assert_equal 5, board.rows
   end
 
   def test_board_cols
     board = Board.new(5,6)
-    assert_equal 6, board.num_cols
+    assert_equal 6, board.cols
   end
 
-  def test_board_start
+  def test_board_set_all_cells_alive
     board = Board.new(3,4)
-    assert board.at(1).at(2)
+    board.set_all_cells_alive
+    assert board.cells[1][2]
   end
 
 end
